8ba4de4d16a54e2e02638699572c231a3a3e6524
tran cong phuoc <teosake.1999@gmail.com>
2021-01-23 12:48:16 +0700

    phuoc up

---
+++ c0720g1-vaccine/src/main/java/com/project/dto/VaccinationByRequestDTO.java
@@ -0,0 +1,105 @@
+package com.project.dto;
+
+public class VaccinationByRequestDTO {
+
+    private Integer patientId;
+    private String name;
+    private String dateOfBirth;
+    private String gender;
+    private String guardian;
+    private String phone;
+    private String address;
+    private String email;
+    private Boolean deleteFlag;
+    private String dateVaccination;
+    private Integer vaccineId;
+
+
+    public String getDateVaccination() {
+        return dateVaccination;
+    }
+
+    public void setDateVaccination(String dateVaccination) {
+        this.dateVaccination = dateVaccination;
+    }
+
+    public Integer getVaccineId() {
+        return vaccineId;
+    }
+
+    public void setVaccineId(Integer vaccineId) {
+        this.vaccineId = vaccineId;
+    }
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Integer patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGuardian() {
+        return guardian;
+    }
+
+    public void setGuardian(String guardian) {
+        this.guardian = guardian;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getDeleteFlag() {
+        return deleteFlag;
+    }
+
+    public void setDeleteFlag(Boolean deleteFlag) {
+        this.deleteFlag = deleteFlag;
+    }
+}
