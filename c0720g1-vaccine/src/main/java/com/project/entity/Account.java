6ce6270ed08281d54a1e92665f41ab8f33439500
lnnaf <vanlinh12b5@gmail.com>
2021-01-23 11:34:32 +0700

    update scurity by linh

--- c0720g1-vaccine/src/main/java/com/project/entity/Account.java
+++ c0720g1-vaccine/src/main/java/com/project/entity/Account.java
@@ -2,21 +2,26 @@ package com.project.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.Set;
 
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "account")
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer accountId;
+    @NotBlank(message = "Please provide a userName")
     private String userName;
+    @NotBlank(message = "Please provide a encryptPw")
     private String encryptPw;
     private String token;
     @OneToOne(mappedBy = "account")
@@ -25,8 +30,13 @@ public class Account {
     private Employee employee;
     @OneToMany(mappedBy = "account")
     @JsonBackReference
-    private Set<AccountRole>accountRoleList;
+    private Set<AccountRole> accountRoleList;
     @OneToMany(mappedBy = "account")
     @JsonBackReference
-    private Set<ImportAndExport>importAndExportList;
+    private Set<ImportAndExport> importAndExportList;
+
+    public Account(String userName, String encryptPw) {
+        this.userName = userName;
+        this.encryptPw = encryptPw;
+    }
 }
