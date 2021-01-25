5331ff471c4c3a7653124e62a77b226b1d94d19a
luyen <luyen2011@gmail.com>
2021-01-25 18:42:36 +0700

    up bai

--- c0720g1-vaccine/src/main/java/com/project/repository/RoleRepository.java
+++ c0720g1-vaccine/src/main/java/com/project/repository/RoleRepository.java
@@ -21,7 +21,7 @@ public interface RoleRepository extends JpaRepository<Role, Integer> {
     @Query(value = "select * from role",nativeQuery = true)
     List<Role> finAllRole();
 
-    
+
     @Modifying
     @Query(value = "insert into account_role(account_id,role_id) values (?1,?2)", nativeQuery = true)
     void setDefaultRole(int accountId, Integer roleId);
